781eecc2ed388745047233baa2a0f408aa33ba1c
testbags <81547926+testbags@users.noreply.github.com>
2021-03-30 20:04:23 -0300

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', ()=> {
-	const contenedor_loader = document.querySelector('.contenedor');
-	contenedor_loader.style.opacity = 0;
-	contenedor_loader.style.visibility = 'hidden'
-})
